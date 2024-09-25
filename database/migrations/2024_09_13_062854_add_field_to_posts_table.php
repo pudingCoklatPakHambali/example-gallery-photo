27e5e229e2f3a149d2be1e9254439708473e9d1b
BungaMekarPontianak <a24409026@gmail.com>
2024-09-25 13:56:20 +0700

    wop: refactoring posts slug

--- database/migrations/2024_09_13_062854_add_field_to_posts_table.php
+++ database/migrations/2024_09_13_062854_add_field_to_posts_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->string('slug');
-        });
+            $table->string('slug')->default('testing');
+        });     
     }
 
     /**
